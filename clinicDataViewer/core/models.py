e13dbcc270eddce85ac593f1f7af8da5a8d0d54b
Thiago Ribeiro Passos <tpassos@192.168.0.10>
2026-09-01 22:26:37 -0300

    feat: adiciona configuracao do banco e migracao inicial do core

--- clinicDataViewer/core/models.py
+++ clinicDataViewer/core/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ClinicalProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='clinical_profile')
+    emergency_phone = models.CharField(max_length=20, blank=True, null=True)
+    blood_type = models.CharField(max_length=3, blank=True, null=True)
+    allergies = models.TextField(blank=True, null=True)
+    illnesses = models.TextField(blank=True, null=True)
+    surgeries = models.TextField(blank=True, null=True)
+    pin_code = models.CharField(max_length=4)
+    qr_code_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Perfil Clínico - {self.user.username}"
